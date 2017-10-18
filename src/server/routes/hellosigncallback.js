10e243a4f7747ecb68f6f8970238d1492ebb64a4
Juan Gabriel <juangabriel@pixeloak.com>
2017-10-17 23:25:33 -0500

    made sure hellosigncallback gets legitimate request by hashing

--- src/server/routes/hellosigncallback.js
+++ src/server/routes/hellosigncallback.js
@@ -1,12 +1,33 @@
+import crypto from 'crypto'
 import express from 'express'
 import multer from 'multer'
+
+import { HELLO_SIGN_API_KEY } from 'server/config'
 
 const router = express.Router()
 const upload = multer()
 
 router.post('/hellosigncallback', upload.any(), (req, res) => {
-  console.log(req.files)
-  res.send("Hello API Event Received")
+  const event = JSON.parse(req.body.json).event
+
+  const {
+    event_time,
+    event_type,
+    event_hash
+  } = event
+
+  const hash = crypto
+    .createHmac('sha256', HELLO_SIGN_API_KEY)
+    .update(event_time + event_type)
+    .digest('hex')
+    .toString()
+
+  if (event_hash === hash) {
+    console.log(event)
+    res.send("Hello API Event Received")
+  } else {
+    res.sendStatus(401)
+  }
 })
 
 export default router
