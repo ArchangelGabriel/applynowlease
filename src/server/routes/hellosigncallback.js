0cdd34c13a51306c875991a737bdb480c5df9482
Juan Gabriel <juangabriel@pixeloak.com>
2017-10-17 23:13:55 -0500

    Try multer for hellosign callback

--- src/server/routes/hellosigncallback.js
+++ src/server/routes/hellosigncallback.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import multer from 'multer'
 
 const router = express.Router()
+const upload = multer()
 
-router.post('/hellosigncallback', (req, res) => {
-  console.log(req)
+router.post('/hellosigncallback', upload.any(), (req, res) => {
+  console.log(req.files)
   res.send("Hello API Event Received")
 })
 
