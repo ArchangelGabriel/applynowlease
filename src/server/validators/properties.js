248f34fd1e18fdbd5800e7107c3aac175db5be86
Gabe Trinidadd <gabe.trinidad@vulcansearch.com>
2018-01-04 20:31:00 -0600

    Code cleanup

--- src/server/validators/properties.js
+++ src/server/validators/properties.js
@@ -24,27 +24,12 @@ export const addProperty = v.compile({
   monthlyAsking: { type: 'number' },
   templateSubject: { type: 'string', optional: true },
   templateDescription: { type: 'string', optional: true },
-  templateId: { type: 'string', optional: true }
+  templateId: { type: 'string', optional: true },
 })
 
 export const applyToProperty = v.compile({ 
   property: 'string',
   applicantName: 'string',
-  status: { type: 'string', enum: ['sent', 'opened', 'viewed', 'pending', 'completed'] }
-  // signers: { 
-  //   type: "array", 
-  //   min: 2, 
-  //   items: {
-  //     type: "object",
-  //     props: {
-  //       email: 'email',
-  //       fullName: 'string',
-  //       phoneNumber: 'string',
-  //       role: { type: 'string', enum: ['Agent', 'Client'] }
-  //     }
-  //   }
-  // },
-  // payerEmail: { type: 'string', optional: true }
 })
 
 export const updateProperty = v.compile({
@@ -71,5 +56,4 @@ export const updateProperty = v.compile({
 export const updateApplication = v.compile({
   applicantName: { type: 'string', optional: true },
   status: { type: 'string', enum: ['sent', 'opened', 'viewed', 'pending', 'completed'], optional: true },
-  // templateId: { type: 'string' },
 })
